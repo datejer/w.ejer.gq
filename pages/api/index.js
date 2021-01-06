ce402c94ab86a3632def22b908e7ab9bf3751016
datejer <ejer.priv@wp.pl>
2021-01-06 10:39:13 +0100

    Change to readdir

--- pages/api/index.js
+++ pages/api/index.js
@@ -4,10 +4,10 @@ import path from "path";
 export default (req, res) => {
 	const dirRelativeToPublicFolder = "img";
 	const dir = path.resolve("./public", dirRelativeToPublicFolder);
-	const filenames = fs.readdirSync(dir);
-	const images = filenames.map((name) =>
-		path.join("/", dirRelativeToPublicFolder, name)
-	);
-
-	res.redirect(images[Math.floor(Math.random() * images.length)]);
+	fs.readdir(dir, (err, files) => {
+		const images = files.map((name) =>
+			path.join("/", dirRelativeToPublicFolder, name)
+		);
+		res.redirect(images[Math.floor(Math.random() * images.length)]);
+	});
 };
